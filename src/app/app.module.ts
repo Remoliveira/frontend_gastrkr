f0ea1479709d4a6e6167f427a5014bab39622c6d
Remo Gresta <remoogg@gmail.com>
2021-08-19 14:28:20 -0300

    aaaaaa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ComentarioComponent } from './comentario/comentario.component';
 import { LoginComponent } from './login/login.component';
 import { ComentarioService } from './comentario.service';
 import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { HomeComponent } from './home/home.component';
     UsuarioComponent,
     ComentarioComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
